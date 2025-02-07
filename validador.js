ee3d6dbdd0305e55b758103003dd30a1ff96c678
Kaique Dias Barroso <diaskaique501@gmail.com>
2025-02-07 18:24:46 -0300

    Update styling and fix VIN validation logic

--- validador.js
+++ validador.js
@@ -1,5 +1,4 @@
 function validarVIN(vin) {
-  // Verifica se o VIN tem 17 caracteres válidos
   if (!/^[A-HJ-NPR-Z0-9]{17}$/i.test(vin)) return false;
 
   const mapaValores = {
@@ -9,36 +8,34 @@ function validarVIN(vin) {
       0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9
   };
   const pesos = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2];
-
+  
   let soma = 0;
   const vinTrated = vin.toUpperCase();
 
-  // Calcular a soma ponderada
   for (let i = 0; i < 17; i++) {
       const caractere = vinTrated[i];
       const valor = mapaValores[caractere];
 
-      if (valor === undefined) return false;
-
+      if (valor === undefined) return false;      
       soma += valor * pesos[i];
   }
 
-  // Calcular o resto da divisão por 11
   const resto = soma % 11;
   
-  // Determina o dígito verificador baseado no resto
   let digitoVerificador = (resto === 10) ? 'X' : resto.toString();
   
   console.log("Dígito verificador esperado:", digitoVerificador);
-  console.log("Dígito verificador no VIN:", vinTrated[9]);  
+  console.log("Dígito verificador no VIN:", vinTrated[8]);  
+ console.log(vinTrated[8]);
 
-  // Verificar se o dígito verificador no VIN corresponde ao calculado
-  return vinTrated[9] === digitoVerificador;
+  return vinTrated[8] === digitoVerificador;
 }
 
 function validar() {
   const vin = document.getElementById('vinInput').value;
   const resultado = validarVIN(vin);
+
+  console.log(resultado)
 
   const resultadoDiv = document.getElementById('resultado');
   if (resultado) {
